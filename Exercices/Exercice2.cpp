dbe95611dad9a2865ebac0d75ded4f93cbaa7242
DivYT <comedhe83@gmail.com>
2024-02-12 04:08:28 +0100

    CPP Exercices

---
+++ Exercices/Exercice2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Multiplicateur!" << std::endl;
+    float a,b  = 0;
+    cout << "Quelle est la valeur de a : " << endl; cin >> a;
+    cout << "Quelle est la valeur de b : " << endl; cin >> b;
+    cout << " La multiplication est " << a*b <<endl;
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 Exercices en C++ corrigés
 
 https://cpp.developpez.com/tutoriels/exercice-corriges-ihm-debutant/
+
+
+Exercices :
+https://www.coodemaroc.com/2021/05/c.html
